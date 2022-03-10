3498d83335e6167c462907be8e766f63910c3bd6
Nayan Islam <nayannk103@gmail.com>
2022-03-11 01:01:40 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// for Mobile Menu//
+
+const navLinks = document.getElementById("mobile_menu");
+const header = document.querySelector(".header");
+// const hero = document.querySelector(".hero");
+function showMenu(){
+    navLinks.style.right = "0px";
+}
+function hideMenu(){
+    navLinks.style.right = "-250px";
+}
+
+
+
+const headerOption = {
+    threshold: .97,
+    rootMargin: "0px",
+  };
+  
+  const headerObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("style");
+      } else {
+        header.classList.remove("style");
+      }
+    });
+  }, headerOption);
+  
+  headerObserver.observe(header);
+  
+  // const heroOption = {
+  //   threshold: 0,
+  //   rootMargin: "0px",
+  // };
+  
+  // const heroObserver = new IntersectionObserver((entries) => {
+  //   entries.forEach((entry) => {
+  //     if (entry.isIntersecting) {
+  //       hero.classList.add("style");
+  //     }
+  //   });
+  // }, heroOption);
+  
+  // heroObserver.observe(hero);
